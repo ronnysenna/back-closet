4e82e4906ca049b41839fcfc50831f0b621bb504
Ronny Senna <ronnysenna@yahoo.com>
2025-09-06 10:40:23 -0300

    Atualiza configuração CORS para incluir novos domínios permitidos

--- src/middleware/cors-config.js
+++ src/middleware/cors-config.js
@@ -3,10 +3,16 @@ import cors from "cors";
 
 // Configuração do CORS para permitir acesso do frontend
 const corsOptions = {
-	origin: ["http://localhost:5173", "http://localhost:3000"],
-	methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true,
+  origin: [
+    "https://closetmodafitness.com",
+    "https://www.closetmodafitness.com",
+    "https://back.closetmodafitness.com",
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 export const corsMiddleware = cors(corsOptions);
